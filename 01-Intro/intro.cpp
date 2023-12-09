ec484a54ffc899570c7bbda28e21965d9790ab92
holgihe <holgihe@eh-herrgen.com>
2023-12-09 02:14:00 +0100

    libdemo added

---
+++ 01-Intro/intro.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World, and everybody ! New again, now as intro..." << std::endl;
+}
---
+++ 03-Libdemo/libdemo.cpp
@@ -0,0 +1,9 @@
+#include "libdemo.h"
+
+int libdemo::add(int a, int b) {
+    return a+b;
+}
+
+float libdemo::add(float a, float b) {
+    return a+b;
+}
